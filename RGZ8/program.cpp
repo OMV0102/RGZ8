364d66ae13345f23644c0d0d2026f407148feb2f
omv0102 <omv98@outlook.com>
2019-05-17 03:55:30 +0700

    бла

--- RGZ8/program.cpp
+++ RGZ8/program.cpp
@@ -15,7 +15,7 @@ int ButtonWidth = 80;
 
 char info[256];
 
-void DLL()
+DWORD WINAPI ThreadFunc(void*)
 {
 	SetWindowText(label, LPCSTR(""));
 
@@ -61,83 +61,78 @@ void DLL()
 		sprintf_s(info, " Библиотека dynamic_lib.dll не найдена!\nПоместите файл dynamic_lib.dll в папку\nс программой и нажмите кнопку \"Обновить данные\"!");
 		SetWindowText(label, LPCSTR(info));
 	}
+	return 0;
 }
 
-LONG WINAPI WndProc(HWND hwnd, UINT Message, WPARAM wparam, LPARAM lparam)
+LRESULT CALLBACK WindowFunc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	switch (Message)
+	switch (msg)
 	{ 
-	case WM_CTLCOLORSTATIC: // выставление свойств перед отрисовкой текстового поля
-	{		
-			HDC hdcStatic = (HDC)wparam;
-			SetTextColor(hdcStatic, RGB(255, 0, 0));
-			SetBkColor(hdcStatic, RGB(0, 0, 0));
-			return (INT_PTR)hBrush;
-	}
-	break;
-	case WM_COMMAND:
-	{
-		if (LOWORD(wparam) == 1) // запуск
-		{
-			HANDLE hThread;
-			DWORD IDThread;
-			hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DLL, NULL, 0, &IDThread); // Запускаем функцию в новом потоке
-			CloseHandle(hThread);
-		}
-		if (LOWORD(wparam) == 2) // выйти
-		{
-			PostQuitMessage(0);
+		case WM_CTLCOLORSTATIC: // выставление свойств перед отрисовкой текстового поля
+		{		
+				HDC hdcStatic = (HDC)wParam;
+				SetTextColor(hdcStatic, RGB(255, 0, 0));
+				SetBkColor(hdcStatic, RGB(0, 0, 0));
+				return (INT_PTR)hBrush;
+				break;
 		}
 		
-		if (LOWORD(wparam) == 3) // очистить окно с текстом
+		case WM_COMMAND:
 		{
-			HANDLE hThread;
-			DWORD IDThread;
-			hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)clear, NULL, 0, &IDThread); // Запускаем функцию в новом потоке
-			CloseHandle(hThread);
+			if (LOWORD(wParam) == 1) // запуск
+			{
+				HANDLE hThread;
+				DWORD IDThread;
+				hThread = CreateThread(NULL, 0, ThreadFunc, NULL, 0, &IDThread); // Запускаем функцию в новом потоке
+				CloseHandle(hThread);
+			}
+			break;
 		}
-		break;
-	}
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hwnd, Message, wparam, lparam);
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
 	return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance,	HINSTANCE hPrevInstance, LPSTR lpCmdLine,int nCmdShow)
+int WINAPI WinMain(HINSTANCE hThisInst,	HINSTANCE hPrevInst, LPSTR str,int nWinMode)
 {
-	WNDCLASS wc;
-	memset(&wc, 0, sizeof(WNDCLASS));
-	wc.lpszClassName = szClassName;
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;			// Обработчик сообщений
-	wc.hInstance = hInstance;
-	wc.hbrBackground = CreateSolidBrush(RGB(200, 200, 200));
-	
-	RegisterClass(&wc);
+	MSG msg;
+	WNDCLASS wcl;
+
+	wcl.hInstance = hThisInst;
+	wcl.lpszClassName = szClassName;
+	wcl.lpfnWndProc = WindowFunc;
+	wcl.style = CS_HREDRAW | CS_VREDRAW;
+	wcl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcl.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcl.lpszMenuName = NULL;
+	wcl.cbWndExtra = 0;
+	wcl.cbClsExtra = 0;
+	wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	RegisterClass(&wcl);
 
 	HDC hDCScreen = GetDC(NULL);
 
 
 	hwnd = CreateWindow(szClassName, szTitle,
 		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-		600, 600, Width, Height, NULL, NULL, hInstance, NULL);
+		600, 600, Width, Height, NULL, NULL, hThisInst, NULL);
 
 	label = CreateWindow("static", "", WS_CHILD | WS_VISIBLE | WS_BORDER | BS_PUSHBUTTON,
-		5, 5, Width - 15, Height * 0.3, hwnd, (HMENU)0, hInstance, NULL);
+		5, 5, Width - 15, Height * 0.3, hwnd, (HMENU)0, hThisInst, NULL);
 
 	button_start = CreateWindow("button", "Обновить данные",
 		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_VCENTER | BS_CENTER, Width - 2 * ButtonWidth - 10, Height - ButtonHeight*3,
-		ButtonWidth, ButtonHeight, hwnd, (HMENU)1, hInstance, NULL);
+		ButtonWidth, ButtonHeight, hwnd, (HMENU)1, hThisInst, NULL);
 
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(hwnd, nWinMode);
 
 	UpdateWindow(hwnd);
 
-	MSG msg;
+	
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
