22625772bca7ef291ae5c36963e81fac3eec8832
omv0102 <omv98@outlook.com>
2019-05-18 15:08:59 +0700

    ГОТОВО

--- RGZ8/compile.bat
+++ RGZ8/compile.bat
@@ -1,5 +1,4 @@
 if exist *.dll del *.dll
-if exist *.exe del *.exe
 cl program.cpp kernel32.lib user32.lib gdi32.lib advapi32.lib
 cl /LD dynamic_lib.cpp kernel32.lib user32.lib gdi32.lib advapi32.lib
 del *.obj *.lib *.exp
